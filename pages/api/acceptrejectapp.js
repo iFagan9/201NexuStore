55ac578c35c7587bf9dc79feb62f0eb42dbdb5e7
Ian Fagan <89874479+iFagan9@users.noreply.github.com>
2023-12-01 15:03:30 -0500

    Finished uploading and deleting submission requests

--- pages/api/acceptrejectapp.js
+++ pages/api/acceptrejectapp.js
@@ -1,4 +1,5 @@
 import clientPromise from "../../lib/mongodb";
+import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
@@ -10,12 +11,22 @@ export default async function handler(req, res) {
         await db.collection('Apps').insertOne({name, developer, image, rating, description, comments, platforms});
         res.status(201).json({message: 'App Acceptance Successfully'});
     } else if (req.method === 'DELETE') {
+        const { ObjectId } = require('mongodb');
         const {id} = req.body;
         const client = await clientPromise;
         const db = client.db('NexuStore');
 
-        await db.collection('Submissions').deleteOne({_id: id});
-        res.status(201).json({message: 'App Deletion Success'})
+        if (!ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'Invalid ObjectId format' });
+        }
+
+        const result = await db.collection('Submissions').findOneAndDelete({ _id: ObjectId(id) });
+
+        if (!result.value) {
+            return res.status(404).json({ message: 'Document not found' });
+        }
+
+        res.status(201).json({ message: 'App Deletion Success' });
 
 
     }
